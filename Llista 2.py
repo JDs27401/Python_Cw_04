1f295d1510278c8af7e2e5ea07048d6a9e4472cc
JDs27401 <s27401@pjwstk.edu.pl>
2025-04-17 21:35:57 +0200

    zrobione zadanie nr 3

--- Llista 2.py
+++ Llista 2.py
@@ -1,18 +1,14 @@
 import string
+import random
 
 #zadanie 1
 # Prosze napisać skrypt, który liczy za użytkownika. Ma on umożliwić użytkownikowi wprowadzenie liczby
 # początkowej, liczby końcowej i wielkości odstępu między podanymi liczbami.
 
 def calcAbs(x: int, y: int):
-    if x < y:
-        return y - x
-    elif y < x:
-        return x - y
-    elif x == y:
-        return 0
+    return abs(x - y)
 
-# print(calcAbs(7, 7))
+# print(calcAbs(5, 7))
 
 #zadanie 2
 # Proszę napisać skrypt, który wczytuje komunikat użytkownika, a następnie wypisuje go w odwrotnej kolejności.
@@ -24,13 +20,43 @@ def reverseInput():
 # reverseInput()
 
 # zadanie 3
+# Proszę napisać grę, w której komputer wybiera losowo słowo, które gracz musi odgadnąć.
+# Komputer informuje gracza, ile liter znajduje się w wybranym słowie.
+# Następnie gracz otrzymuje pięć szans na zadanie pytania, czy jakaś litera jest zawarta w tym słowie.
+# Komputer może odpowiedzieć tylko "tak"lub "nie". Gracz musi odgadnąć słowo.
 
+def guessTheWord():
+    words = []
+    try:
+        with open("words.txt", "r") as write:
+            file = write
+            for line in file:
+              words.append(line.strip())
+    except FileNotFoundError:
+        print('Could not find file named words.txt')
 
+    wordToGuess = words[random.randint(0, len(words) - 1)]
+    print('The word is {} letters long.'.format(len(wordToGuess)))
+    # print(wordToGuess)
+
+    lifes = 5
+    while lifes > 0:
+        strin = input('Guess word: ')
+        if strin == wordToGuess:
+            print('You guessed it!')
+            return
+        else:
+            lifes -= 1
+            print('Incorrect! {} life(s) remain.'.format(lifes))
+    print('You lost!')
+    # print(words)
+
+# guessTheWord()
 
 #zadanie 4
 def hangman():
-    word= "blablabla"
-    numberOfTries= 10
+    word = "blablabla"
+    numberOfTries = 10
 
     result = ['_' for i in word]
     print(result)
@@ -99,8 +125,8 @@ def XO():
 
     while moves < 9:
 
-        strin1 = input()
-        strin2 = input()
+        strin1 = input('Column: ')
+        strin2 = input('Row: ')
 
         if game[rows[strin2]][cols[strin1]] == '-':
             game[rows[strin2]][cols[strin1]] = player
@@ -116,4 +142,14 @@ def XO():
         else:
             player = 'X'
 
-XO()
+# XO()
+
+
+
+#zadanie 8
+# Napisz zgodnie z zasadami programowania obiektowego program, który umieszcza na przekątnej tablicy 10 × 10 liczby losowe od 0 do 9, a poza nią zera.
+# Dodatkowo program oblicza sumę liczb znajdujących się na przekątnej. W programie skorzystaj z metod: zapelnijTablicę() i obliczSume().
+# Pierwsza z nich wypełnia przekątną tablicy liczbami losowymi od 0 do 9; natomiast druga oblicza sumę liczb na przekątnej.
+
+#trzeba napisać klasę matrix, a następnie dodać do niej 2 metody opsiane powyrzej
+# def matrix():
